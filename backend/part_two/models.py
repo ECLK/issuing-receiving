a305f7170f79a69c8808a3cc42b4598a11a2badf
Thivi <theviyanthan@outlook.com>
2020-03-10 00:11:24 +0530

    Add part_two app

---
+++ backend/part_two/models.py
@@ -0,0 +1,42 @@
+from django.db import models
+
+# Create your models here.
+
+
+class IssuedToSPO(models.Model):
+    issued_time = models.DateTimeField()
+    no_of_stamps = models.IntegerField()
+    no_of_pens = models.IntegerField()
+    spo = models.ForeignKey("staffs.Staffs", on_delete=models.SET_NULL)
+    election = models.ForeignKey(
+        "election.Election", on_delete=models.SET_NULL)
+    entered_time = models.DateTimeField()
+    id = models.AutoField()
+
+    class Meta:
+        unique_together = (("entered_time", "id"))
+
+
+class BallotBoxesIssued(models.Model):
+    serial_number = models.CharField()
+    issued_to_spo = models.ForeignKey("IssuedToSPO", on_delete=models.CASCADE)
+
+
+class ReceivedFromSPO(models.Model):
+    received_time = models.DateTimeField()
+    no_of_stamps = models.IntegerField()
+    no_of_pens = models.IntegerField()
+    spo = models.ForeignKey("staffs.Staffs", on_delete=models.SET_NULL)
+    election = models.ForeignKey(
+        "election.Election", on_delete=models.SET_NULL)
+    entered_time = models.DateTimeField()
+    id = models.AutoField()
+
+    class Meta:
+        unique_together = (("entered_time", "id"))
+
+
+class BallotBoxesReceived(models.Model):
+    serial_number = models.CharField()
+    received_from_spo = models.ForeignKey(
+        "ReceivedFromSPO", on_delete=models.CASCADE)
