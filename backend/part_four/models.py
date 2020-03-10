914aae9820c9cbd111dcac4d377f7da5280967f2
Thivi <theviyanthan@outlook.com>
2020-03-11 02:03:19 +0530

    Modify part_four model

--- backend/part_four/models.py
+++ backend/part_four/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+
+
+class IssuedToCCO(models.Model):
+    issued_time = models.DateTimeField()
+    cco = models.ForeignKey("staffs.Staffs", on_delete=models.SET_NULL)
+    i_r_aro = models.ForeignKey("staffs.Staffs", on_delete=models.SET)
+    v = models.IntegerField()
+    l = models.IntegerField()
+    g = models.IntegerField()
+    g1 = models.IntegerField()
+    z = models.IntegerField()
+    election = models.ForeignKey("election.Election", on_delete=models.CASCADE)
+    entered_time = models.DateTimeField()
+    id = models.AutoField()
+
+    class Meta:
+        unique_together = (("id", "entered_time"))
+
+
+class BallotBoxesIssuedToCCO:
+    issued_cco = models.ForeignKey("IssuedToCCO", on_delete=models.CASCADE)
+    serial_number = models.CharField()
