cc604162e4a8d789486eb4acd16e93bb7b22dbb7
Thivi <theviyanthan@outlook.com>
2020-03-11 02:02:14 +0530

    Modify Staffs model

--- backend/staffs/models.py
+++ backend/staffs/models.py
@@ -8,3 +8,18 @@ class Staffs(models.Model):
     address = models.CharField(max_length=255)
     nic = models.CharField(max_length=255)
     staff_type = models.CharField(max_length=255)
+    id = models.AutoField()
+    election = models.ForeignKey(
+        "election.Election", on_delete=models.SET_NULL)
+
+    class Meta:
+        unique_together = (("id", "election"))
+
+
+class IRAROPollingDistricts(models.Model):
+    aro = models.ForeignKey("staffs.Staffs", on_delete=models.CASCADE)
+    polling_district = models.ForeignKey(
+        "units.PollingDistricts", on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together: (("aro", "polling_district"))
