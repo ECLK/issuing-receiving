08c1dc494ab70feea9624fe0600c6a1d6dc4869d
Thivi <theviyanthan@outlook.com>
2020-03-13 01:34:15 +0530

    Format code

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,16 @@
 import React from "react";
 import { BrowserRouter, Switch, Redirect, Route } from "react-router-dom";
 import { Authentication } from "./helpers/authentication";
-import { routes, RouteInterface } from "./configs";
+import { routes } from "./configs";
 import { ProtectedRoute } from "./helpers";
+import { RouteInterface } from "./shared/models/routes";
 
 export const App = (): React.ReactElement => {
 	return (
 		<BrowserRouter>
 			<Authentication>
 				<Switch>
-					<Redirect exact={true} path="/" to="" />
+					<Redirect exact={true} path="/" to="/dashboard" />
 					{routes?.map((route: RouteInterface) => {
 						if (route.protected) {
 							return <ProtectedRoute path={route.path} exact={route.exact} component={route.component} />;
--- frontend/src/helpers/auth-context.tsx
+++ frontend/src/helpers/auth-context.tsx
@@ -1,4 +1,5 @@
 import React from "react";
-import { AuthContextInterface, initialState } from "./authentication";
+import { AuthContextInterface, AuthState } from "../shared/models";
 
+const initialState: AuthState = { authenticated: false, accessToken: null };
 export const AuthContext = React.createContext<AuthContextInterface>({ authState: initialState, dispatch: () => {} });
