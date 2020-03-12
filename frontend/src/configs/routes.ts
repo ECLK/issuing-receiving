d65a8cae324230a2e7c10fc5977b752f7e03a80d
Thivi <theviyanthan@outlook.com>
2020-03-13 01:33:59 +0530

    Add routes to the dashboard and the sign-in page

--- frontend/src/configs/routes.ts
+++ frontend/src/configs/routes.ts
@@ -1,7 +1,22 @@
-export interface RouteInterface {
-	component: React.FunctionComponent | React.ComponentClass;
-	path: string;
-	exact: boolean;
-	protected: boolean;
-}
-export const routes: RouteInterface[] = [];
+import { RouteInterface } from "../shared/models/routes";
+import { SignIn } from "../pages/authentication";
+import { Dashboard } from "../pages/dashboard";
+
+export const routes: RouteInterface[] = [
+	{
+		component: SignIn,
+		path: "/login",
+		showOnMenu: false,
+		protected: false,
+		exact: true,
+		name: "Login"
+	},
+	{
+		component: Dashboard,
+		path: "/dashboard",
+		showOnMenu: true,
+		protected: true,
+		exact: true,
+		name: "Home"
+	}
+];
