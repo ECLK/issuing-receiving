047c38d6bdc0bfc7278c156ddb9587a4e8fd5995
Thivi <theviyanthan@outlook.com>
2020-03-14 02:44:04 +0530

    Add sign out feature

--- frontend/src/configs/routes.ts
+++ frontend/src/configs/routes.ts
@@ -1,19 +1,29 @@
 import { RouteInterface } from "../shared/models/routes";
 import { SignIn } from "../pages/authentication";
 import { Dashboard } from "../pages/dashboard";
+import { SignOut } from "../pages/authentication/sign-out";
+import { LOGOUT, LOGIN, HOME } from "../shared/constants";
 
 export const routes: RouteInterface[] = [
 	{
 		component: SignIn,
-		path: "/login",
+		path: LOGIN,
 		showOnMenu: false,
 		protected: false,
 		exact: true,
 		name: "Login"
 	},
 	{
+		component: SignOut,
+		path: LOGOUT,
+		protected: true,
+		exact: true,
+		name: "Logout",
+		showOnMenu: false
+	},
+	{
 		component: Dashboard,
-		path: "/dashboard",
+		path: HOME,
 		showOnMenu: true,
 		protected: true,
 		exact: true,
