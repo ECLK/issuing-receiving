5d8cd118feb5bc66591fbae1cb1536c19eba0f83
Thivi <theviyanthan@outlook.com>
2020-03-14 02:40:44 +0530

    Create App Layout

--- frontend/src/theme/index.ts
+++ frontend/src/theme/index.ts
@@ -1,9 +1,12 @@
 import { makeStyles, Theme } from "@material-ui/core/styles";
 import { signInGrid, signInPaper } from "./sign-in";
+import { appBar, drawer } from "./app-layout";
 
 const useStyles = makeStyles((theme: Theme) => ({
 	signInGrid: signInGrid(theme),
-	signInPaper: signInPaper(theme)
+	signInPaper: signInPaper(theme),
+	appBar: appBar(theme),
+	drawer: drawer(theme)
 }));
 
 export default useStyles;
