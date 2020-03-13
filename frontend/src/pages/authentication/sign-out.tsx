53b27ceb0286bd2aabfbdc13eecbc729d80554a7
Thivi <theviyanthan@outlook.com>
2020-03-14 02:44:04 +0530

    Add sign out feature

---
+++ frontend/src/pages/authentication/sign-out.tsx
@@ -0,0 +1,16 @@
+import React, { useEffect, useContext } from "react";
+import { signOut } from "../../utils";
+import { AuthContext } from "../../helpers";
+import { useHistory } from "react-router-dom";
+import { LOGIN } from "../../shared/constants";
+
+export const SignOut = (): React.ReactElement => {
+	const { dispatch } = useContext(AuthContext);
+
+	const history = useHistory();
+	useEffect(() => {
+		signOut(dispatch);
+		history.push(LOGIN);
+	}, [history, dispatch]);
+	return <>Logout</>;
+};
