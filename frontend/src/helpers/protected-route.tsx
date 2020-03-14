9c04c4edb41993f38873d7a8db03728be7282d65
Thivi <theviyanthan@wso2.com>
2020-03-15 03:11:21 +0530

    Fix routing issues

--- frontend/src/helpers/protected-route.tsx
+++ frontend/src/helpers/protected-route.tsx
@@ -1,18 +1,24 @@
 import React, { useContext } from "react";
 import { Route, Redirect } from "react-router-dom";
 import { AuthContext } from ".";
+import { updateCallbackUrl } from "../utils";
+import { AppLayout } from "../layout";
 
 interface ProtectedRoutePropsInterface {
 	path: string;
 	component: React.FunctionComponent | React.ComponentClass;
 	exact: boolean;
+	appLayout: boolean;
 }
 export const ProtectedRoute = (props: ProtectedRoutePropsInterface): React.ReactElement => {
-	const { path, component, exact } = props;
+	const { path, component, exact, appLayout } = props;
 	const { authState } = useContext(AuthContext);
 
+	updateCallbackUrl(path);
+
 	if (authState.authenticated) {
-		return <Route path={path} component={component} exact={exact} />;
+		const route = <Route path={path} component={component} exact={exact} />;
+		return appLayout ? <AppLayout>{route}</AppLayout> : route;
 	} else {
 		return <Redirect to="/login" />;
 	}
