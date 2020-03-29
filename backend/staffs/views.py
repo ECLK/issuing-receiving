f9833412cb07472f205b009e1521bd461d06ba8d
Thivi <theviyanthan@wso2.com>
2020-03-30 00:50:21 +0530

    Add staffs apis

--- backend/staffs/views.py
+++ backend/staffs/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from staffs.models import Staffs, IRAROPollingDistricts, PDStorageInCharge
+from staffs.serializers import StaffsSerializer, IRAROSerializer, PDStorageInChargeSerializer
 
 # Create your views here.
+
+class StaffsViewSet(viewsets.ModelViewSet):
+    queryset = Staffs.objects.all()
+    serializer_class = StaffsSerializer
+    
+class IRAROViewSet(viewsets.ModelViewSet):
+    queryset = IRAROPollingDistricts.objects.all()
+    serializer_class = IRAROSerializer
+
+class PDStorageInChargeViewSet(viewsets.ModelViewSet):
+    queryset = PDStorageInCharge.objects.all()
+    serializer_class = PDStorageInChargeSerializer
