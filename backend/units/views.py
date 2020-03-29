f2e73b2fcc1595ec3e09f60f77d91acfe0c63671
Thivi <theviyanthan@wso2.com>
2020-03-30 00:50:11 +0530

    Add units apis

--- backend/units/views.py
+++ backend/units/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from units.serializers import ElectoralDistrictSerializer, AdministrativeDistrictSerializer, PollingDivisionSerializer, PollingDistrictSerializer, PollingStationSerializer, CountingCentreSerializer
+from units.models import ElectoralDistrict, AdministrativeDistrict, PollingDivision, PollingDistrict, PollingStation, CountingCentre
 
 # Create your views here.
+
+
+class ElectoralDistrictViewSet(viewsets.ModelViewSet):
+    queryset = ElectoralDistrict.objects.all()
+    serializer_class = ElectoralDistrictSerializer
+
+
+class AdministrativeDistrictViewSet(viewsets.ModelViewSet):
+    queryset = AdministrativeDistrict.objects.all()
+    serializer_class = AdministrativeDistrictSerializer
+
+
+class PollingDivisionViewSet(viewsets.ModelViewSet):
+    queryset = PollingDivision.objects.all()
+    serializer_class = PollingDivisionSerializer
+
+
+class PollingDistrictViewSet(viewsets.ModelViewSet):
+    queryset = PollingDistrict.objects.all()
+    serializer_class = PollingDistrictSerializer
+
+
+class PollingStationViewSet(viewsets.ModelViewSet):
+    queryset = PollingStation.objects.all()
+    serializer_class = PollingStationSerializer
+
+
+class CountingCentreViewSet(viewsets.ModelViewSet):
+    queryset = CountingCentre.objects.all()
+    serializer_class = CountingCentreSerializer
