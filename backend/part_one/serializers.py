810b7d22a1ed05c423f97732bc702c02ae5d6375
Thivi <theviyanthan@wso2.com>
2020-03-30 01:15:50 +0530

    Add part_one apis

---
+++ backend/part_one/serializers.py
@@ -0,0 +1,28 @@
+from rest_framework import serializers
+from part_one.models import ReportedToWorkElectionDay, ReportedToWorkBeforeElection
+from staffs.serializers import StaffsSerializer
+
+
+class ReportedToWorkElectionDayReadSerializer(serializers.ModelSerializer):
+    staff = StaffsSerializer(read_only=True)
+
+    class Meta:
+        model = ReportedToWorkElectionDay
+        fields = ["id", "time", "staff","i_r_aro"]
+
+class ReportedToWorkElectionDayWriteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ReportedToWorkElectionDay
+        fields = ["id", "time", "staff", "i_r_aro"]
+
+class ReportedToWorkBeforeReadSerializer(serializers.ModelSerializer):
+    staff = StaffsSerializer(read_only=True)
+    
+    class Meta:
+        model = ReportedToWorkBeforeElection
+        fields = ["id", "time", "staff", "i_r_aro"]
+
+class ReportedToWorkBeforeWriteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ReportedToWorkBeforeElection
+        fields=["id","time","staffs","i_r_aro"]
--- backend/units/urls.py
+++ backend/units/urls.py
@@ -6,7 +6,7 @@ router.register("electoral_district", ElectoralDistrictViewSet)
 router.register("administrative_district", AdministrativeDistrictViewSet)
 router.register("polling_division", PollingDivisionViewSet)
 router.register("polling_district", PollingDistrictViewSet)
-router.register("polling_station", PollingStationViewSet)
-router.register("counting_centre", CountingCentreViewSet)
+router.register(r'election/(?P<election>.+)/polling_station', PollingStationViewSet)
+router.register(r'election/(?P<election>.+)/counting_centre', CountingCentreViewSet)
 
 urlpatterns = router.urls
