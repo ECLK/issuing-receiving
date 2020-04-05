8eb3c3b42b778d3eccbdc7af162598b72ca7ff4b
Thivi <theviyanthan@wso2.com>
2020-04-06 02:44:26 +0530

    Add permissions to part one API

--- backend/part_one/models.py
+++ backend/part_one/models.py
@@ -1,16 +1,16 @@
 from django.db import models
 from election.models import Election
-from staffs.models import Staffs, IRAROPollingDistricts
+from staffs.models import Staffs, IRARO
 # Create your models here.
 
 
 class ReportedToWorkElectionDay(models.Model):
     time = models.TimeField()
     election = models.ForeignKey(
-        Election, on_delete=models.SET_NULL, null=True)
+        Election, on_delete=models.CASCADE)
     staff = models.ForeignKey(Staffs, on_delete=models.CASCADE)
     i_r_aro = models.ForeignKey(
-        IRAROPollingDistricts, on_delete=models.CASCADE, related_name="reported_to_work_aro_election")
+        IRARO, on_delete=models.CASCADE, related_name="reported_to_work_aro_election")
 
     class Meta:
         constraints = [
@@ -24,7 +24,7 @@ class ReportedToWorkBeforeElection(models.Model):
         Election, on_delete=models.SET_NULL, null=True)
     staff = models.ForeignKey(Staffs, on_delete=models.CASCADE)
     i_r_aro = models.ForeignKey(
-        IRAROPollingDistricts, on_delete=models.CASCADE, related_name="reported_to_work_aro_before")
+        IRARO, on_delete=models.CASCADE, related_name="reported_to_work_aro_before")
 
     class Meta:
         constraints = [
--- backend/part_one/views.py
+++ backend/part_one/views.py
@@ -1,4 +1,6 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from rest_framework.permissions import BasePermission
 from part_one.models import ReportedToWorkElectionDay, ReportedToWorkBeforeElection
 from part_one.serializers import (
     ReportedToWorkElectionDayWriteSerializer,
@@ -9,6 +11,18 @@ from part_one.serializers import (
 
 # Create your views here.
 
+class IsOwner(BasePermission):
+    message = "You don't have permission to access this"
+
+    def has_permission(self, request, view):
+        user = request.user
+        if user.is_authenticated:
+            aro = user.aro
+            id = view.kwargs.get("pk")
+            if id is None:
+                return True
+            return ReportedToWorkElectionDay.objects.get(pk=id).i_r_aro == aro
+        return False
 
 class ReportedToWorkElectionDayViewSet(viewsets.ModelViewSet):
     queryset = ReportedToWorkElectionDay.objects.all()
@@ -17,10 +31,30 @@ class ReportedToWorkElectionDayViewSet(viewsets.ModelViewSet):
         'list': ReportedToWorkElectionDayReadSerializer,
         'retrieve': ReportedToWorkElectionDayReadSerializer
     }
+    permission_classes=[IsOwner]
 
     def get_queryset(self):
-        election = self.kwargs['election']
-        return ReportedToWorkElectionDay.objects.filter(election__id=election)
+        user = self.request.user
+        aro = user.aro
+        return ReportedToWorkElectionDay.objects.filter(i_r_aro=aro)
+
+    def create(self, request):
+        try:
+            user = self.request.user
+            if user.is_authenticated:
+                aro = user.aro
+                election = aro.election
+                data = request.data.copy()
+                data["election"] = election.id
+                data["i_r_aro"] = aro.id
+                report_to_work_serializer = self.get_serializer_class()(data = data)
+                if (report_to_work_serializer.is_valid()):
+                    report_to_work_serializer.save()
+                    return Response(report_to_work_serializer.data, status=status.HTTP_201_CREATED)
+                return Response(status=status.HTTP_400_BAD_REQUEST)
+            return Response(status=status.HTTP_401_UNAUTHORIZED)
+        except:
+            return Response(status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
     def get_serializer_class(self):
         try:
