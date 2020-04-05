8f4a86a5091fea338b5c1043b7f558480de096ad
Thivi <theviyanthan@wso2.com>
2020-04-06 02:43:04 +0530

    Add authentication

--- backend/issuing_receiving_api/settings.py
+++ backend/issuing_receiving_api/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+from .authentication import BearerAuthentication
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -39,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
+    'drf_yasg',
+    'rest_framework.authtoken',
     'election',
     'staffs',
     'units',
@@ -128,3 +131,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        "issuing_receiving_api.authentication.BearerAuthentication",
+        "rest_framework.authentication.BasicAuthentication"
+    ),
+}
