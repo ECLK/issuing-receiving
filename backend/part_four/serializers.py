f463f592777a05e06cb6b5d67ee9b76dba7b4b72
Thivi <theviyanthan@wso2.com>
2020-04-13 01:26:31 +0530

    Add id to the fields of part_four serializers

--- backend/part_four/serializers.py
+++ backend/part_four/serializers.py
@@ -4,12 +4,12 @@ from part_four.models import IssuedToCCO, BallotBoxesIssuedToCCO
 class IssuedToCCOSerializer(serializers.ModelSerializer):
     class Meta:
         model = IssuedToCCO
-        fields = ["issued_time", "cco", "i_r_aro",
+        fields = ["id", "issued_time", "cco", "i_r_aro",
                   "v", "l", "g", "g1", "z", "election", "entered_time"]
 
 
 class BallotBoxesIssuedToCCOSerializer(serializers.ModelSerializer):
     class Meta:
         model = BallotBoxesIssuedToCCO
-        fields = ["issued_cco", "serial_number"]
+        fields = ["id", "issued_cco", "serial_number"]
 
