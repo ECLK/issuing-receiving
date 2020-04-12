5636d38fa4e4053a9471864606f7ab8bcd737365
Thivi <theviyanthan@wso2.com>
2020-04-13 01:29:26 +0530

    Add Owner permission check and mixins to part_three views

--- backend/part_three/views.py
+++ backend/part_three/views.py
@@ -1,13 +1,22 @@
 from rest_framework import viewsets
 from part_three.models import ReceivedFromSPO
-from part_three.serializers import ReceivedFromSPOSerializer
+from part_three.serializers import ReceivedFromSPOThreeSerializer
+from utils.permissions import IsOwnerVerify
+from utils.mixins import AddElectionAROMixin
+from rest_framework.permissions import BasePermission
+
+
+class IsOwner(BasePermission):
+    message = "You don't have permission to access this"
+
+    def has_permission(self, request, view):
+        is_owner = IsOwnerVerify(ReceivedFromSPO)
+        return is_owner.verify(request, view)
 
 # Create your views here.
 
-class ReceivedFromSPOViewSet(viewsets.ModelViewSet):
-    queryset = ReceivedFromSPO.objects.all()
-    serializer_class = ReceivedFromSPOSerializer
 
-    def get_serializer_class(self):
-        election = self.kwargs['election']
-        return ReceivedFromSPO.objects.filter(election__id=election)
+class ReceivedFromSPOViewSet(AddElectionAROMixin, viewsets.ModelViewSet):
+    queryset = ReceivedFromSPO.objects.all()
+    serializer_class = ReceivedFromSPOThreeSerializer
+    permission_classes = [IsOwner]
