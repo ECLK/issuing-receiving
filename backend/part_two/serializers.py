fd8f3f98e920c9b3ca5c4f58a3f161243263e37b
Thivi <theviyanthan@wso2.com>
2020-04-13 01:27:29 +0530

    Add id to the fields of part_two serializers

--- backend/part_two/serializers.py
+++ backend/part_two/serializers.py
@@ -9,26 +9,26 @@ from part_two.models import (
 class IssuedToSPOSerializer(serializers.ModelSerializer):
     class Meta:
         model = IssuedToSPO
-        fields = ["issued_time", "no_of_stamps",
+        fields = ["id", "issued_time", "no_of_stamps",
                   "no_of_pens", "spo", "i_r_aro", "election", "entered_time"]
 
 class ReceivedFromSPOSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = ReceivedFromSPO
-        fields = ["received_time", "no_of_stamps",
+        fields = ["id", "received_time", "no_of_stamps",
                   "no_of_pens", "spo", "i_r_aro", "election", "entered_time"]
 
 class BallotBoxesIssuedSerializer(serializers.ModelSerializer):
     class Meta:
         model = BallotBoxesIssuedToSPO
-        fields = ["serial_number", "spo", "i_r_aro",
+        fields = ["id", "serial_number", "spo", "i_r_aro",
                   "election", "entered_time", "issued_time"]
 
 class BallotBoxesReceivedSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = BallotBoxesReceived
-        fields = ["serial_number", "spo", "i_r_aro",
+        fields = ["id", "serial_number", "spo", "i_r_aro",
                   "election", "entered_time", "received_time"]
                   
