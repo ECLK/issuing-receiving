c7ab32d9bbf8150cab111560c5b6840d55bdec88
Thivi <theviyanthan@wso2.com>
2020-04-13 01:28:28 +0530

    Override create method to allow nested write. Check user unique constraint.

--- backend/staffs/serializers.py
+++ backend/staffs/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from staffs.models import Staffs, IRAROPollingDistricts, PDStorageInCharge, IRARO
 from django.contrib.auth.models import User
+from django.contrib.auth.validators import UnicodeUsernameValidator
 
 
 class StaffsSerializer(serializers.ModelSerializer):
@@ -9,18 +10,49 @@ class StaffsSerializer(serializers.ModelSerializer):
         fields = ["nic", "address", "name", "id"]
 
 
+class UserNoPasswordSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["username"]
+
+    def validate_username(self, value):
+        if User.objects.get(username=value) is None:
+            return value
+        else:
+            if User.objects.get(username=value).id == self.parent.instance.user.id:
+                return value
+            else:
+                raise serializers.ValidationError(
+                    "The username already exists.")
+
+
 class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ["username", "password", "is_superuser", "is_staff"]
-        extra_kwargs = {'password': {'write_only': True}}
+        fields = ["username", "password"]
+        extra_kwargs = {
+            'password': {
+                'write_only': True
+            },
+            "username": {
+                "validators": [UnicodeUsernameValidator]
+            }
+        }
 
     def create(self, validated_data):
+        validated_data["is_superuser"] = False
+        validated_data["is_staff"] = False
         user = User.objects.create_user(**validated_data)
         return user
 
-class IRAROSerializer(serializers.ModelSerializer):
+    def update(self, instance, validated_data):
+        instance.username = validated_data.get("username", instance.username)
+        instance.save()
+        return instance
+
+
+class IRAROCreateSerializer(serializers.ModelSerializer):
     user = UserSerializer()
 
     class Meta:
@@ -31,8 +63,31 @@ class IRAROSerializer(serializers.ModelSerializer):
         user_data = validated_data.pop("user")
         user = self.fields["user"].create(user_data)
         validated_data["user"] = user
-        iraro = IRARO.objects.create(**validated_data)
-        return iraro
+        try:
+            iraro = IRARO.objects.create(**validated_data)
+            return iraro
+        except:
+            User.objects.get(User).delete()
+
+class IRAROSerializer(serializers.ModelSerializer):
+    user = UserNoPasswordSerializer()
+    
+    class Meta:
+        model = IRARO
+        fields=["staff", "election", "user", "id"]
+    def update(self, instance, validated_data):
+        user_data = validated_data.pop("user")
+        user = self.fields["user"].update(instance.user, user_data)
+        user_copy = instance.user
+        instance.user = user
+        try:
+            instance.staff = validated_data.get("staff", instance.staff)
+            instance.election = validated_data.get(
+                "election", instance.election)
+            instance.save()
+            return instance
+        except:
+            user_copy.save()
 
 
 class IRAROPollingDistrictsSerializer(serializers.ModelSerializer):
@@ -44,4 +99,4 @@ class IRAROPollingDistrictsSerializer(serializers.ModelSerializer):
 class PDStorageInChargeSerializer(serializers.ModelSerializer):
     class Meta:
         model = PDStorageInCharge
-        fields = ["in_charge", "polling_division","election","id"]
+        fields = ["in_charge", "polling_division", "election", "id"]
