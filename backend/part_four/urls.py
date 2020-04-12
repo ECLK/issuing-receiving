07d5193af7edd166d3d9ef0ee457eb54cd961de7
Thivi <theviyanthan@wso2.com>
2020-04-13 01:28:41 +0530

    Remove election query param from urls

--- backend/part_four/urls.py
+++ backend/part_four/urls.py
@@ -2,7 +2,7 @@ from rest_framework import routers
 from part_four.views import IssuedToCCOViewSet, BallotBoxesIssuedToCCOViewSet
 
 router = routers.DefaultRouter()
-router.register(r'election/(?P<election>.*)/issued-to-cco', IssuedToCCOViewSet)
-router.register(r'election/(?P<election>.*)/issued-ballot-boxes-to-cco', BallotBoxesIssuedToCCOViewSet)
+router.register(r'issued-to-cco', IssuedToCCOViewSet)
+router.register(r'issued-ballot-boxes-to-cco', BallotBoxesIssuedToCCOViewSet)
 
 urlpatterns = router.urls
--- backend/part_three/urls.py
+++ backend/part_three/urls.py
@@ -2,6 +2,6 @@ from part_three.views import ReceivedFromSPOViewSet
 from rest_framework import routers
 
 router = routers.DefaultRouter()
-router.register(r'election/(?P<election>.*)/received-from-spo', ReceivedFromSPOViewSet)
+router.register(r'received-from-spo', ReceivedFromSPOViewSet)
 
 urlpatterns=router.urls
