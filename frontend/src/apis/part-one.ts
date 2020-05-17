0a6d36743a4c0f45fe5a3943446c2d8dd3f04a50
Thivi <theviyanthan@wso2.com>
2020-05-17 22:42:57 +0530

    Add on election APIs

--- frontend/src/apis/part-one.ts
+++ frontend/src/apis/part-one.ts
@@ -88,3 +88,88 @@ export const deleteBeforeElection = (id: number):Promise<any> => {
             return Promise.reject(error?.response?.data);
         });
 }
+
+export const listOnElections = (): Promise<any> => {
+    const config: AxiosRequestConfig = {
+        url: apiEndpoints.partOne.onElectionDay,
+        method: Methods.GET
+    };
+
+    return http(config)
+        .then((response) => {
+            if (response.status !== 200) {
+                return Promise.reject(errorStatus(response.status));
+            }
+            const reportToWork = response.data as ReportToWork<true>[];
+
+            reportToWork.forEach((value: ReportToWork<true>, index: number, array: ReportToWork<true>[]) => {
+                const date = new Date(value.time);
+                array[index].time = date.toLocaleTimeString();
+                array[index].date = date.toLocaleDateString();
+                array[index].timestamp = date.getTime();
+            });
+            return Promise.resolve(response.data as ReportToWork<true>);
+        })
+        .catch((error) => {
+            Promise.reject(error);
+        });
+};
+
+export const createOnElection = (data: ReportToWork<false>): Promise<any> => {
+    const config: AxiosRequestConfig = {
+        url: apiEndpoints.partOne.onElectionDay,
+        method: Methods.POST,
+        data: data
+    };
+
+    return http(config)
+        .then((response) => {
+            if (response.status !== 201) {
+                return Promise.reject(errorStatus(response.status));
+            }
+
+            return Promise.resolve(response.data);
+        })
+        .catch((error) => {
+            return Promise.reject(error?.response?.data);
+        });
+};
+
+export const updateOnElection = (id: number, data: ReportToWork<false>): Promise<any> => {
+    const config: AxiosRequestConfig = {
+        url: `${apiEndpoints.partOne.onElectionDay}${id}/`,
+        method: Methods.PUT,
+        data: data
+    };
+
+    return http(config)
+        .then((response) => {
+            if (response.status !== 200) {
+                return Promise.reject(errorStatus(response.status));
+            }
+
+            return Promise.resolve(response.data);
+        })
+        .catch((error) => {
+            return Promise.reject(error?.response?.data);
+        });
+};
+
+export const deleteOnElection = (id: number): Promise<any> => {
+    const config: AxiosRequestConfig = {
+        url: `${apiEndpoints.partOne.onElectionDay}${id}/`,
+        method: Methods.DELETE
+    };
+
+    return http(config)
+        .then((response) => {
+            if (response.status !== 204) {
+                return Promise.reject(errorStatus(response.status));
+            }
+
+            return Promise.resolve(response.data);
+        })
+        .catch((error) => {
+            return Promise.reject(error?.response?.data);
+        });
+};
