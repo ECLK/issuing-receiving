a8f42789b343e7bb200e8178f1a374a500ff28d8
Thivi <theviyanthan@wso2.com>
2020-05-17 22:31:28 +0530

    Add createStaff api

--- frontend/src/apis/staffs.ts
+++ frontend/src/apis/staffs.ts
@@ -2,6 +2,7 @@ import { http, Methods } from "../configs";
 import { apiEndpoints } from "../constants";
 import { AxiosRequestConfig } from "axios";
 import { errorStatus } from "../utils";
+import { Staff } from "../models";
 
 export const listStaffs = (): Promise<any> => {
     const config = {
@@ -16,6 +17,25 @@ export const listStaffs = (): Promise<any> => {
 
         return Promise.resolve(response.data);
     }).catch(error => {
-        return Promise.reject(error);
+        return Promise.reject(error?.response?.data);
     })
 };
+
+
+export const createStaff = (data: Staff): Promise<any> => {
+    const config = {
+        url: apiEndpoints.staffs.staffs,
+        method: Methods.POST,
+        data
+    }
+
+    return http(config).then((response) => {
+        if (response.status !== 201) {
+            return Promise.reject(errorStatus(response.status));
+        }
+
+        return Promise.resolve(response.data);
+    }).catch(error => {
+        return Promise.reject(error ?.response ?.data);
+    })
+}
